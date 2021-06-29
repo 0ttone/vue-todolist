0858c246a633d8568d219fe893409448eb358edf
MyUsername <emanuele.marini37@gmail.com>
2021-06-29 17:24:05 +0200

    ripasso bootstrap

--- js/script.js
+++ js/script.js
@@ -20,13 +20,20 @@ const app = new Vue(
 
 
           mounted () {
-                const element = document.getElementById('newTaskElement');
-                element.focus();
+                this.autofocus();
           },
 
 
 
           methods: {
+                  //riduciamo a funzione per sfruttarlo dopo ogni inserimento o cancellazione
+                  autofocus: function (){
+                        const element = document.getElementById('newTaskElement');
+                element.focus();
+                  },
+
+
+
                 //creiamo addTask semplicemente costruendo una funzione che 
                 // usi il dato newTask pushandolo nell array todolist - non serve
                 //altro alla funzione , abbiamo già dato immesso con input e array da popolare
@@ -35,6 +42,7 @@ const app = new Vue(
                       this.toDoList.push(this.newTask);
                       //con questa istruzione ripuliamo il campo di input
                       this.newTask = '';
+                      this.autofocus();
                 },
 
             //     creiamo la funzione deleteTask sfruttando splice e
@@ -43,8 +51,14 @@ const app = new Vue(
                         console.log(index)
                         //non è necessario assegnarla si autodichiara
                         //splice agisce sull originale come push 
+
+                        //come taglia, vediamo gli argomenti?
+                        //a: start (indexnel nsotro caso), 
+                        //b: quanti elementi cancello a partire da start
+                        //senza b mi ridà l array tagliato dal punto indicato in start 
                         this.toDoList.splice(index, 1)
-                  }
+                        this.autofocus();
+                  },
 
 
                   
